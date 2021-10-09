684a6fd38a1e5516da1ee6706191987312cf22fa
Kyle Klein <KyleAKlein@gmail.com>
2021-10-08 21:31:49 -0700

    added

---
+++ additional_prob_1.rb
@@ -0,0 +1,10 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+flintstones_hash = {}
+
+#Turn this array into a hash where the names 
+#are the keys and the values are the positions in the array
+# create empty hash
+# create key
+# create value = index
+# iterate over each element until everything is added
