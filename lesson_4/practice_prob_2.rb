6f40f3512180fb6eb487c7d4039a102fe88d883e
Kyle Klein <KyleAKlein@gmail.com>
2021-10-08 21:29:16 -0700

    added

--- lesson_4/practice_prob_2.rb
+++ lesson_4/practice_prob_2.rb
@@ -1,8 +1,8 @@
 ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 
-def total_age(hsh)
-	p hsh.values.sum
-
+def total_age(sum)
+	sum.values.sum
 end
 
-total_age(ages)
+
+p total_age(ages)
