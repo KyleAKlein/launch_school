d37bd7d9282b4e69e4e7a8541291b1f93147e623
Kyle Klein <KyleAKlein@gmail.com>
2021-10-13 12:14:19 -0700

    more exercises

---
+++ lesson_5/practice_problem_9.rb
@@ -0,0 +1,11 @@
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+arr_1 = arr[0].sort.reverse
+arr_2 = arr[1].sort.reverse
+arr_3 = arr[2].sort.reverse
+
+new_arr = []
+new_arr << arr_1
+new_arr << arr_2
+new_arr << arr_3
+
