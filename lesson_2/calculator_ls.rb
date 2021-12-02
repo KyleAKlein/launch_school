29724488711787ebff17a1077888e20c0eb5cb68
Kyle Klein <KyleAKlein@gmail.com>
2021-12-02 06:39:26 -0800

    more exercises

--- lesson_2/calculator_ls.rb
+++ lesson_2/calculator_ls.rb
@@ -1,4 +1,4 @@
- ruby def prompt(message)
+def prompt(message)
 	Kernel.puts("=> #{message}")
 end
 
--- small_problems/medium_1_4.rb
+++ small_problems/medium_1_4.rb
@@ -1,7 +1,36 @@
+def initialize_lights(x)
+  lights = Hash.new
+  1.upto(x) {|num| lights[num] = "off" }
+  lights
+end
 
+def flip_switch(lights, nth)
+  lights.each do |position, state|
+    if position % nth == 0
+      lights[position] = (state == "off")? "on" : "off"
+    end
+  end
+end
+
+def lights_on(lights)
+  lights.select { |_, state| state == "on" }.keys
+end
+
+def toggle_lights(n)
+  lights = initialize_lights(n)
+  1.upto(lights.size) do |iteration_number|
+    flip_switch(lights, iteration_number)
+  end
+
+  lights_on(lights)
+end
+
+p toggle_lights(1000)
 
 # Row of lights
 # toggle light
+# lights on array
+
 
 # round 1: every light is turned on
 # round 2: lights 2 and 4 are now off; 1, 3, 5 are on
